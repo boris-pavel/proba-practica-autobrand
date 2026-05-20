e25e63ad0b5a630c86afc00d8321447c0b85b9d5
boris <boris@DESKTOP-OSO9R2P>
2026-05-20 07:26:21 +0300

    test: add ActiveProfiles annotation for test context configuration

--- src/test/java/ro/autobrand/proba/ProbaApplicationTests.java
+++ src/test/java/ro/autobrand/proba/ProbaApplicationTests.java
@@ -2,12 +2,12 @@ package ro.autobrand.proba;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ProbaApplicationTests {
-
 	@Test
 	void contextLoads() {
 	}
-
 }
